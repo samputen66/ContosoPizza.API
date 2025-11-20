5dd94c49c2c88cac7c9d312e09d38eed3d8b5860
Jeremy Camacho <jeremy.y.camacho@accenture.com>
2025-11-20 16:16:17 +0800

    initial commit

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,71 @@
+﻿using ContosoPizza.API.Models;
+using ContosoPizza.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetById(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+        // POST action
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+            return CreatedAtAction(nameof(GetById), new { id = pizza.Id }, pizza);
+        }
+        // PUT action
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+        // DELETE action
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+   
+            if (pizza is null)
+                return NotFound();
+       
+            PizzaService.Delete(id);
+   
+            return NoContent();
+        }
+
+    }
+}
